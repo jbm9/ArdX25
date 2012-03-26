8b5799666cf3fc05c6f1c96f916adc80968e8451
Josh Myer <josh@joshisanerd.com>
2012-03-25 18:47:52 -0700

    One more pass for documentation/cleanup over test/test_aprs_weather.cpp

--- test/test_aprs_weather.cpp
+++ test/test_aprs_weather.cpp
@@ -7,7 +7,6 @@
 
 SUITE(APRSWeather) {
 
-
   struct APRSWeatherFixture {
     GPSHandler gpsh;
     APRSWeather aw;
@@ -21,58 +20,38 @@ SUITE(APRSWeather) {
   };
 
 
+  TEST_FIXTURE(APRSWeatherFixture, TestTemperatures) {
 
-
-
-
-  TEST_FIXTURE(APRSWeatherFixture, TestInitialization)
-    {
-      CHECK(!gpsh.lock);
-    }
-
-  TEST_FIXTURE(APRSWeatherFixture, TestBytewiseIngestionSimple)
-    {
-      const char *gpgga = "$GPGGA,021741,3745.5748,N,12224.9648,W,2,09,1.0,20.2,M,-32.1,M,,*48\n";
-      int i;
-
-      for (i = 0; i < strlen(gpgga)-1; i++) {
-	CHECK(!gpsh.saw(gpgga[i]));
-      }
-      CHECK(gpsh.saw(gpgga[i]));
-    }
-
-  TEST_FIXTURE(APRSWeatherFixture, TestTemperatures)
-    {
 #if USE_RAW_3V3_AREF
-      aw.note_temp_internal(199);
-      aw.note_temp_external(150);
-      CHECK(aw.temp_internal_fahrenheit() == 2);
-      CHECK(aw.temp_external_fahrenheit() == -112);
+    aw.note_temp_internal(199);
+    aw.note_temp_external(150);
+    CHECK(aw.temp_internal_fahrenheit() == 2);
+    CHECK(aw.temp_external_fahrenheit() == -112);
 
-      aw.note_temp_internal(252);
-      CHECK(aw.temp_internal_fahrenheit() == 125);
+    aw.note_temp_internal(252);
+    CHECK(aw.temp_internal_fahrenheit() == 125);
 
-      aw.note_temp_internal(255);
-      CHECK(aw.temp_internal_fahrenheit() == 125);
+    aw.note_temp_internal(255);
+    CHECK(aw.temp_internal_fahrenheit() == 125);
 
-      aw.note_temp_external(231);
-      CHECK(aw.temp_external_fahrenheit() == 76);
+    aw.note_temp_external(231);
+    CHECK(aw.temp_external_fahrenheit() == 76);
 #else
-      aw.note_temp_internal(199);
-      aw.note_temp_external(150);
-      CHECK(aw.temp_internal_fahrenheit() == -18);
-      CHECK(aw.temp_external_fahrenheit() == -126);
+    aw.note_temp_internal(199);
+    aw.note_temp_external(150);
+    CHECK(aw.temp_internal_fahrenheit() == -18);
+    CHECK(aw.temp_external_fahrenheit() == -126);
 
-      aw.note_temp_internal(252);
-      CHECK(aw.temp_internal_fahrenheit() == 98);
+    aw.note_temp_internal(252);
+    CHECK(aw.temp_internal_fahrenheit() == 98);
 
-      aw.note_temp_internal(255);
-      CHECK(aw.temp_internal_fahrenheit() == 104);
+    aw.note_temp_internal(255);
+    CHECK(aw.temp_internal_fahrenheit() == 104);
 
-      aw.note_temp_external(231);
-      CHECK(aw.temp_external_fahrenheit() == 51);
+    aw.note_temp_external(231);
+    CHECK(aw.temp_external_fahrenheit() == 51);
 #endif
-    }
+  }
 
 
   TEST_FIXTURE(APRSWeatherFixture, TestPressure) {
@@ -84,32 +63,31 @@ SUITE(APRSWeather) {
   }
 
 
-  TEST_FIXTURE(APRSWeatherFixture, TestFormatString)
-    {
-      char buf[1024];
-      uint16_t pos;
+  TEST_FIXTURE(APRSWeatherFixture, TestFormatString)  {
+    char buf[1024];
+    uint16_t pos;
 
 
-      const char *gpgga = "$GPGGA,021741,3745.5748,N,12224.9648,W,2,09,1.0,20.2,M,-32.1,M,,*48\n";
-      int i;
+    const char *gpgga = "$GPGGA,021741,3745.5748,N,12224.9648,W,2,09,1.0,20.2,M,-32.1,M,,*48\n";
+    int i;
 
-      for (i = 0; i < strlen(gpgga)-1; i++) {
-	CHECK(!gpsh.saw(gpgga[i]));
-      }
-      CHECK(gpsh.saw(gpgga[i]));
-
-
-      aw.note_temp_external(231);
-      aw.note_pressure(140);
-
-      pos = aw.format_string(buf, 1024);
-
-      const char *expected_packet = ";AJ9BM-11 *021700h3745.57N/12224.96W_.../...t051b07000";
-      const uint16_t expected_len = strlen(expected_packet);
-
-      CHECK(pos == expected_len);
-      CHECK(0 == strncmp(expected_packet, buf, expected_len));
+    for (i = 0; i < strlen(gpgga)-1; i++) {
+      CHECK(!gpsh.saw(gpgga[i]));
     }
+    CHECK(gpsh.saw(gpgga[i]));
+
+
+    aw.note_temp_external(231);
+    aw.note_pressure(140);
+
+    pos = aw.format_string(buf, 1024);
+
+    const char *expected_packet = ";AJ9BM-11 *021700h3745.57N/12224.96W_.../...t051b07000";
+    const uint16_t expected_len = strlen(expected_packet);
+
+    CHECK(pos == expected_len);
+    CHECK(0 == strncmp(expected_packet, buf, expected_len));
+  }
 
 }
 
